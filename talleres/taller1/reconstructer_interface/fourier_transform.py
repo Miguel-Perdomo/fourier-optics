c5b7b46c15b7294005ea480d45aa57f44363d787
VelasquezE <132410679+VelasquezE@users.noreply.github.com>
2025-09-16 18:14:01 -0500

    Delete unnecesary visualization

--- talleres/taller1/reconstructer_interface/fourier_transform.py
+++ talleres/taller1/reconstructer_interface/fourier_transform.py
@@ -7,8 +7,6 @@ import scipy.fft
 img_path = "/mnt/c/Users/estev/OneDrive - Universidad Nacional de Colombia/Universidad/02_Física/SemestreVIII/ÓpticaDeFourier/FourierOptics_assignments/talleres/taller1/data/foto00_480_480.jpg"
 img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
-h, w = img.shape
-
 sample = 7
 
 def take_sample(img: np.ndarray, sample_size: int) -> np.ndarray:
@@ -32,9 +30,3 @@ fourier_transform = scipy.fft.fft2(img_sample)
 shifted_spectrum = scipy.fft.fftshift(fourier_transform)
 
 visualization = np.abs(shifted_spectrum)
-
-#plt.imshow(visualization, cmap = "gray")
-#plt.savefig("resultado.pdf", dpi = 400)
-#cv2.imshow("test", visualization)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
